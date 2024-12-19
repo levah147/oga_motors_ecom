870d94f035cf84e14156aafdfb587383d542da1c
levah Emmanuel <emmanuelnater@gmail.com>
2024-12-19 22:11:35 +0100

    i add requirements.txt file

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -85,6 +85,18 @@ DATABASES = {
 }
 
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
